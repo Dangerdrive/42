b682c5fdd9be6afaa633a55e4533ec3d9662b7d5
Felippe De alencar silva <fde-alen@c1r1p3.42sp.org.br>
2023-04-17 18:27:41 -0300

    01

---
+++ BasecampReloaded/ex06/ft_print_alphabet.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fde-alen <fde-alen@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/17 15:41:59 by fde-alen          #+#    #+#             */
+/*   Updated: 2023/04/17 15:42:05 by fde-alen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print_alphabet(void)
+{
+	char	alpha;
+
+	alpha = 'a';
+	while (alpha <= 'z')
+	{
+		ft_putchar(alpha);
+		alpha++;
+	}
+}
---
+++ BasecampReloaded/ex07/ft_print_numbers.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_numbers.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fde-alen <fde-alen@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/17 15:42:57 by fde-alen          #+#    #+#             */
+/*   Updated: 2023/04/17 15:43:34 by fde-alen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print_numbers(void)
+{
+	char	number;
+
+	number = '0';
+	while (number <= '9')
+	{
+		ft_putchar(number);
+		number++;
+	}
+}
