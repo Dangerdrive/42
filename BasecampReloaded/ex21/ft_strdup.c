fd987cc78750d81a3ba2fc2c16b8e2784dfcda94
Felippe De alencar silva <fde-alen@c1r3p4.42sp.org.br>
2023-04-21 18:38:07 -0300

    5

--- BasecampReloaded/ex21/ft_strdup.c
+++ BasecampReloaded/ex21/ft_strdup.c
@@ -1,23 +1,57 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fde-alen <fde-alen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/17 15:41:59 by fde-alen          #+#    #+#             */
-/*   Updated: 2023/04/21 14:37:18 by fde-alen         ###   ########.fr       */
+/*   Updated: 2023/04/21 16:45:43 by fde-alen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	ft_putchar(char c);
+#include <stdlib.h>
 
-int	ft_strcmp(char *s1, char *s2)
+int	ft_strlen(char *str)
 {
-	while (*s1 && (*s1 == *s2))
+	int	i;
+
+	i = 0;
+	while (str[i])
 	{
-		s1++;
-		s2++;
+		i++;
 	}
-	return (*s1 - *s2);
+	return (i);
 }
+
+int	*ft_range(int min, int max)
+{
+	size_t	n;
+	int	i;
+
+	if (min >= max)
+		return (NULL);
+	n = max - min - 1;
+	i = 0;
+	while ((min + i) < max)
+	{
+		arr[i] = min + i;
+		i++;
+	}
+}
+	// char	*dest;
+	// char	*temp;
+	// size_t	len;
+
+	// len = ft_strlen(src);
+	// dest = malloc(len + 1);
+	// if (dest == NULL)
+	// 	return (NULL);
+	// temp = dest;
+	// while (*src)
+	// {
+	// 	*temp++ = *src++;
+	// }
+	// *temp = '\0';
+	// return (dest);
+
